b4d000df8cf2ca2f7a49077083ab509b67a031ec
famesensor <sathit.ju@outlook.com>
2023-04-02 16:33:41 +0700

    styled: update format code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { EventEmitterModule } from '@nestjs/event-emitter'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [EventEmitterModule.forRoot()],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
