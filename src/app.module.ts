53d9011413b520b26d381b1e0781aaa1695616e2
famesensor <sathit.ju@outlook.com>
2023-04-10 22:28:22 +0700

    feat: add imports config, event emitter module, order module, notificate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { ConfigModule } from '@nestjs/config'
 import { EventEmitterModule } from '@nestjs/event-emitter'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { OrderModule } from './order/order.module'
+import { NotificationModule } from './notification/notification.module'
 
 @Module({
-    imports: [ConfigModule.forRoot(), EventEmitterModule.forRoot()],
+    imports: [
+        ConfigModule.forRoot(),
+        EventEmitterModule.forRoot(),
+        OrderModule,
+        NotificationModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
